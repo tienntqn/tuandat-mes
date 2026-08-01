427fbabe0b333a7c897af9719f390480a6e7b376
Tien Nguyen <tienntqn@gmail.com>
2026-08-01 10:17:04 +0700

    Fix: 400

--- apps/api/src/modules/plan/soho-converter.controller.ts
+++ apps/api/src/modules/plan/soho-converter.controller.ts
@@ -7,6 +7,7 @@ import {
   StreamableFile,
   UploadedFile,
   UseInterceptors,
+  ValidationPipe,
 } from '@nestjs/common'
 import { FileInterceptor } from '@nestjs/platform-express'
 import { memoryStorage } from 'multer'
